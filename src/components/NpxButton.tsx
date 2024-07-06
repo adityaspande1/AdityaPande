1690d64ef43ff9d76564f0a1e8a884472d62331b
adityaspande1 <121101849+adityaspande1@users.noreply.github.com>
2024-07-06 16:53:17 +0530

    Update NpxButton.tsx

--- src/components/NpxButton.tsx
+++ src/components/NpxButton.tsx
@@ -8,22 +8,23 @@ import {easeOut, motion} from "framer-motion"
 // }
 
 export default function NpxButton() {
-    const handleClick = () => {
-        navigator.clipboard.writeText('npx devaditya');
-        toast('Make sure to run in your terminal 😄!');
-      };
+    
   return (
     <>
      <Toaster/>
     <motion.button
-      onClick={handleClick}
+      onClick={() => {
+        navigator.clipboard.writeText('npx devaditya');
+        toast('Make sure to run in your terminal 😄!');
+      }}
       className=" shim-button inline-flex py-2 px-2 sm:py-4 md:py-6 sm:px-7 md:px-9 animate-shim
        items-center text-2xl justify-center rounded-md
         border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)]
          bg-[length:200%_100%] font-medium text-slate-400 transition-colors 
          focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
           focus:ring-offset-slate-50 gap-2
-          mt-10  -ml-40 mr-12 mb-40
+          mt-10  sm:-ml-40 mr-12 mb-40
+          ml-10
           "
           whileHover={{scale:1.05}}
           whileTap={{scale:0.95}}
