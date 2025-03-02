b287351d199b3a7962a31db4e68f387943b579d1
adityaspande1 <121101849+adityaspande1@users.noreply.github.com>
2025-03-02 23:38:28 +0530

    achivements fixup

--- src/data/Achievements.tsx
+++ src/data/Achievements.tsx
@@ -7,12 +7,12 @@ export const achievements = [
       year: "DEC 2024",
       rank: "Top 5 In India",
       icon: <Award className="text-[#10b981] mb-2" size={24} />,
-      description: "Built a Few Shot Language Agnostic Keyword Spotting System",
+      description: "Built a Few Shot Language Agnostic Keyword Spotting System.",
       status: "completed"
     },
     {
       id: 2,
-      title: "IISF-SIF Hackathon Finalist",
+      title: " IISF-SIF Hackathon Finalist",
       year: "JAN 2024",
       rank: "Top 50 in India",
       icon: <Award className="text-[#10b981] mb-2" size={24} />,
@@ -21,7 +21,7 @@ export const achievements = [
     },
     {
       id: 3,
-      title: "TechFiest Finalist",
+      title: " PICT TechFiest Finalist",
       year: " Mar 2024",
       rank: "Top 5 out of 30 teams",
       icon: <Award className="text-[#10b981] mb-2" size={24} />,
